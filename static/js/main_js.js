a472a45aa223f714ef0bcbe9c7a7d698f0d7c8ba
HaEun9505 <0104whgkdms@naver.com>
2021-12-16 12:59:32 +0900

    board hit

--- static/js/main_js.js
+++ static/js/main_js.js
@@ -2,6 +2,7 @@
 var pic = document.getElementById('pic');
 pic.onmouseover = changePic;  //함수 호출 ()괄호 생략
 pic.onmouseout = originPic;
+setInterval(myWatch, 1000);
 
 function changePic(){  //사진 변경
     pic.src = "../static/images/healing.jpg";
@@ -9,4 +10,10 @@ function changePic(){  //사진 변경
 
 function originPic(){  //원래 사진으로 변경
     pic.src = "../static/images/activity.jpg";
+}
+
+function myWatch(){
+    var date = new Date();  //날짜.시간 생성
+    var now = date.toLocaleTimeString();  //시간 표현
+    document.getElementById("demo").innerHTML = now;
 }
